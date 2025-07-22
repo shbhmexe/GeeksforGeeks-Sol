3ebc88aa251d0a43d13545b37d418dd622847c05
Shubham shukla <shubhushukla586@gmail.com>
2025-07-22 23:38:30 +0530

    Add solutions for missing number and parenthesis checker
    Added SmallestPositiveMissing.cpp for finding the smallest missing positive integer and ParenthesisChecker.cpp for checking balanced parentheses. These files implement efficient algorithms for their respective problems.

---
+++ easy/ParenthesisChecker.cpp
@@ -0,0 +1,17 @@
+//Question 222
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        //CodeGenius
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
+            else{
+                if(!st.empty() && ((st.top()=='(' && s[i]==')') || (st.top()=='[' && s[i]==']')||
+                (st.top()=='{' && s[i]=='}'))) st.pop();
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
