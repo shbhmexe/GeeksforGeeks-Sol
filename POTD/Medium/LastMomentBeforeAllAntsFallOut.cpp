8387d5508a003d4797ee303ab6cfebd932810a60
Shubham shukla <shubhushukla586@gmail.com>
2025-07-24 22:54:47 +0530

    Add solution for Last Moment Before All Ants Fall Out
    Implements the getLastMoment function to determine the last moment before all ants fall out of a plank, based on their initial positions and directions.

---
+++ POTD/Medium/LastMomentBeforeAllAntsFallOut.cpp
@@ -0,0 +1,18 @@
+//Question 225
+
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // shbhm
+        
+        int ans = 0;
+        for(int i = 0; i<left.size(); i++)
+        ans = max(ans, left[i]);
+        
+        for(int i = 0; i<right.size(); i++)
+        ans = max(ans,n - right[i]);
+        
+        return ans;
+        
+    }
+};
