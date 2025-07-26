9d99c56f0d91cdbde4e8d5f8aef73f1f8832be02
Shubham shukla <shubhushukla586@gmail.com>
2025-07-26 23:44:36 +0530

    Create MaxCircularSubarraySum.cpp

---
+++ POTD/Hard/MaxCircularSubarraySum.cpp
@@ -0,0 +1,45 @@
+//question 240
+
+class Solution {
+  public:
+    int kadanesMax(vector<int>& nums, int n) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+        
+        for(int i = 1; i < n; i++) {
+            sum = max(sum + nums[i], nums[i]);
+            maxSum = max(maxSum, sum);
+        }
+        
+        return maxSum;
+    }
+    
+    int kadanesMin(vector<int>& nums, int n) {
+        int sum = nums[0];
+        int minSum = nums[0];
+        
+        for(int i = 1; i < n; i++) {
+            sum = min(sum + nums[i], nums[i]);
+            minSum = min(minSum, sum);
+        }
+        
+        return minSum;
+    }
+    
+    int maxCircularSum(vector<int>& arr) {
+        int n = arr.size();
+        int SUM = accumulate(begin(arr), end(arr), 0);
+        
+        int minSum = kadanesMin(arr, n);
+        int maxSum = kadanesMax(arr, n);
+        
+        int circSum = SUM - minSum;
+
+        // If all elements are negative, max circular sum will be 0, which is incorrect
+        if (maxSum > 0) {
+            return max(maxSum, circSum);
+        }
+
+        return maxSum;
+    }
+};
