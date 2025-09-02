b7615c0b02340a302022d0fee9098f316e518679
Shubham shukla <shubhushukla586@gmail.com>
2025-09-02 22:30:33 +0530

    Add ContainerWithMostWater solution
    Implements the maxWater function using a two-pointer approach to solve the Container With Most Water problem. Calculates the maximum area between two lines in the input vector.

---
+++ POTD/Medium/ContainerWithMostWater.cpp
@@ -0,0 +1,27 @@
+//Question 397
+class Solution {
+
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int i = 0, j = n-1;
+        
+        int water = 0;
+        while(i<j) {
+            
+            //start from the smallest one and calculate water
+            int h = min(arr[i], arr[j]);
+            int w = j-i;
+            int area = h*w;
+            water = max(water, area);
+            
+            if(arr[i] < arr[j])
+                i++;
+            else
+                j--;
+        }
+        
+        return water;
+    }
+};
