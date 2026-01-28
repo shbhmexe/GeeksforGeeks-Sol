3d3c6cfc9178c71d85bb8f6d55719d4055b1637d
shbhmexe <shubhushukla586@gmail.com>
2026-01-28 12:06:09 +0530

    Add solutions for permutation and word search problems
    Added GeneratePermutationsofanarray.cpp implementing array permutation generation using next_permutation, and WordSearch.cpp implementing a DFS-based word search in a 2D grid. These files provide solutions for questions 709 and 707 respectively.

---
+++ POTD/Medium/GeneratePermutationsofanarray.cpp
@@ -0,0 +1,13 @@
+//Question 709
+class Solution {
+  public:
+    vector<vector<int>> permuteDist(vector<int>& arr) {
+        // code here
+        vector<vector<int>>ans;
+        sort(arr.begin() , arr.end());
+        do{
+            ans.push_back(arr);
+        }while(next_permutation(arr.begin() , arr.end()));
+        return ans;
+    }
+};`
