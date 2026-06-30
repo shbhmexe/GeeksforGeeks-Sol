9ff8bd058ef871ad1cbf9789b20e9debe0f79245
shbhmexe <shubhushukla586@gmail.com>
2026-06-30 22:56:46 +0530

    Add solution for LeetCode question

---
+++ POTD/Hard/CountSubarrayswithgivenXOR.cpp
@@ -0,0 +1,16 @@
+//Question 111 june26
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+         //CodeGenius
+        unordered_map<int,int>mp;
+        int preXor=0,count=0;
+        for(int i=0;i<arr.size();i++){
+            preXor^=arr[i];
+            if(preXor==k) count++;
+            if(mp.find(preXor^k)!=mp.end()) count+=mp[preXor^k];
+            mp[preXor]++;
+        }
+        return count;
+    }
+};
